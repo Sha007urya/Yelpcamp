782c9594356da3535662edde5abb792da0d42cc6
Shaurya <shauryaraghuva@gmail.com>
2024-06-16 05:23:53 +0530

    in the final phase

--- app.js
+++ app.js
@@ -16,11 +16,12 @@ const Review=require('./models/review');
 const passport=require('passport');
 const LocalStrategy=require('passport-local');
 const User=require('./models/user');
+const helmet=require('helmet');
 
 const campgroundsRoutes=require('./routes/campgrounds');
 const reviewsRoutes=require('./routes/reviews');
 const userRoutes=require('./routes/users');
-
+const mongoSanitize=require('./models/user')
 mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -34,7 +35,11 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')));
+app.use(mongoSanitize(
+     {replaceWith:'_'}
+));
 const sessionConfig={
+     name:'session',
      secret:'thisshouldbeabettersecret!',
      resave:false,
      saveUninitialized:true,
@@ -48,6 +53,7 @@ app.use(session(sessionConfig));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(helmet());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
